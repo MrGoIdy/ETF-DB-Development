edff7e0d51a17403942328c82f35e83837a763f7
MrGoIdy <zolotarev.me@mail.ru>
2017-12-05 14:47:24 +0300

    Started Options added

---
+++ PathToSaveGetter/PathToSaveGetter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathToSaveGetter
+{
+    public static class PathToSaveGetter
+    {
+        public static string GetPathFromFile_PathToParsedDataTXT()
+        {
+            return File.ReadAllText(Environment.CurrentDirectory + "/PathToParsedData.txt");
+        }
+
+        public static string GetPathFromFile_PathToParsedDataTXT(string path)
+        {
+            return File.ReadAllText(path + "/PathToParsedData.txt");
+        }
+
+        public static string GetPathFromFile(string path, string fileName)
+        {
+            return File.ReadAllText(path + "/PathToParsedData.txt");
+        }
+
+        public static string GetPathFromFile(string fullPath)
+        {
+            return File.ReadAllText(fullPath);
+        }
+
+
+    }
+}
